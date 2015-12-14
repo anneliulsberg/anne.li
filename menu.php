73ac5392d2dc70903e58b57a7a3977830743a063
anneliulsberg <anneli@ulsberg.no>
2015-12-14 23:01:10 +0100

    added a menu to include in all files

---
+++ menu.php
@@ -0,0 +1,10 @@
+<nav>
+	<label for="menu-toggle"></label>  
+	<input type="checkbox" id="menu-toggle"/>
+	<ul>
+		<li><a href="/">Home</a></li>
+		<li><a href="/#portfolio">Portfolio</a></li>
+		<li><a href="/#tools">Tools</a></li>
+		<li><a href="/#contact">Contact</a></li>
+	</ul>
+</nav>
