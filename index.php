d11654427ae3da0c920d8867e816112b178afc73
anneliulsberg <anneli@ulsberg.no>
2016-01-06 12:51:34 +0100

    Simplified and cleaned up the html

--- index.php
+++ index.php
@@ -10,103 +10,67 @@
 	
 	<body>
 		<?php include 'menu.php'; ?>
-		
-<!--	  <div class="debug">
-	    <label><input type="checkbox"> Debug</label>
-	  </div>-->
-	
-	  <div class="parallax">
-	    <div id="group1" class="parallax__group">
-	      <div class="parallax__layer parallax__layer--base">
-	        <div class="title">
-	        	<div id="content"><h1>Anneli</h1>
-		        	<h2>Interaction designer</h2>
-		        	<p>I am a creative person who enjoys to unfold my creativity either digitally or hands on with paint, brushes and canvas.</p>
-		        	<p>I espessially like to use my creativity to make beautiful, userfriendly and interactive web designs.</p>
-		        </div>
-	        </div>
-	      </div>
-	    </div>
-	    
-	    <div id="group2" class="parallax__group">
-	      <div class="parallax__layer parallax__layer--base">
-	        <div class="title">
-	        	<h3>Creativity is my game</h3>
-	        </div>
-	      </div>
-	    </div>
-	    
-	    <div id="group3" class="parallax__group">
-	      <div class="parallax__layer parallax__layer--base">
-	        <div class="title" id="portfolio">
-				<h4>Portfolio</h4>
-				<p>These projects portray my skills in responsive design, CSS, HTML and illustration</p>
-				
-				<ul class="projects">
-					<li><a href="/projects/oppenfotografi.php"><span>Oppen Fotografi</span></a></li>
-					<li><a href="/projects/mikroaktiv.php"><span>Mikroaktiv</span></a></li>
-					<li><a href="/projects/florian.php"><span>Florian</span></a></li>
-					<li><a href="/projects/kinetic.php"><span>Kinetic typography</span></a></li>
-					<li><a href="/projects/kommoden.php"><span>Kommoden med det rare i</span></a></li>
-					<li><a href="/projects/16dager.php"><span>16 dager med aktivisme</span></a></li>
-					<li><a href="/projects/infographic-wow.php"><span>Infographic - WoW</span></a></li>
-					<li><a href="/projects/go.html"><span>go!</span></a></li>
-					<li><a href="/projects/iyengaryoganorge.php"><span>Iyengar yoga Norge</span></a></li>
-					<li><a href="/projects/kgartneri.php"><span>Kjær Gartneri</span></a></li>
-				</ul>
-	        </div>
-	      </div>
-	    </div>
-	    
-	    <div id="group4" class="parallax__group">
-	      <div class="parallax__layer parallax__layer--deep">
-	        <div class="title"><!--Deep Background Layer--></div>
-	      </div>
-	    </div>
-	    
-	    <div id="group5" class="parallax__group">
-	      <div class="parallax__layer parallax__layer--base">
-	        <div class="title" id="tools">
-	        	<h3>Tools</h3>
-        		<p>This is the list of the tools I use while working on projects and which I have the most experience with</p>
-	        	<ul>
-	        		<li><span>Adobe Photoshop</span></li>
-	        		<li><span>Adobe Illustrator</span></li>
-	        		<li><span>Adobe After Effects</span></li>
-	        		<li><span>Espresso</span></li>
-	        		<li><span>GitHub</span></li>
-	        		<li><span>Wacom Bamboo</span></li>
-	        	</ul>   
-			</div>
-	      </div>
-	    </div>
-	    
-	    <div id="group6" class="parallax__group">
-	      <div class="parallax__layer parallax__layer--base">
-	      	<div id="footer-content">
-		        <div class="title" id="contact">
-					<h3>Contact</h3>
-					<p>You can reach me on one or more of these places</p>
-					<ul>
-						<li><a href="https://no.linkedin.com/pub/anneli-ulsberg/97/287/88"><span>Linkedin</span></a></li>
-						<li><a href="https://www.facebook.com/anneli.ulsberg"><span>Facebook</span></a></li>
-						<li><a href="mailto:anneli@ulsberg.no"><span>mail</span></a></li>
-						<li><a href="https://github.com/anneliulsberg"><span>Github</span></a></li>
-						<li><a href="https://twitter.com/litntweet"><span>Twitter</span></a></li>
-					</ul>	     
-		        </div>
-		  	</div>
-	      </div>
-	    </div>
 
-<!--	  <script>
-	    var debugInput = document.querySelector("input");
-	    function updateDebugState() {
-	        document.body.classList.toggle('debug-on', debugInput.checked);
-	    }
-	    debugInput.addEventListener("click", updateDebugState);
-	    updateDebugState();
-	  </script>-->
+		<div class="parallax">
+			<div class="parallax-group">
+				<div class="layer"></div>
+			</div>	
+		
+		<div class="content" id="about">
+			<h1>Anneli</h1>
+			<h2>Interaction designer</h2>
+			<p>I am a creative person who enjoys to unfold my creativity either digitally or hands on with paint, brushes and canvas.</p>
+			<p>I espessially like to use my creativity to make beautiful, userfriendly and interactive web designs.</p>
+		</div>
+
+		
+		<div class="content" id="slogan">
+			<h3>Creativity is my game</h3>
+		</div>
+		
+		<div class="content" id="portfolio">
+			<h4>Portfolio</h4>
+			<p>These projects portray my skills in responsive design, CSS, HTML and illustration</p>
+			
+			<ul class="projects">
+				<li><a href="/projects/oppenfotografi.php"><span>Oppen Fotografi</span></a></li>
+				<li><a href="/projects/mikroaktiv.php"><span>Mikroaktiv</span></a></li>
+				<li><a href="/projects/florian.php"><span>Florian</span></a></li>
+				<li><a href="/projects/kinetic.php"><span>Kinetic typography</span></a></li>
+				<li><a href="/projects/kommoden.php"><span>Kommoden med det rare i</span></a></li>
+				<li><a href="/projects/16dager.php"><span>16 dager med aktivisme</span></a></li>
+				<li><a href="/projects/infographic-wow.php"><span>Infographic - WoW</span></a></li>
+				<li><a href="/projects/go.html"><span>go!</span></a></li>
+				<li><a href="/projects/iyengaryoganorge.php"><span>Iyengar yoga Norge</span></a></li>
+				<li><a href="/projects/kgartneri.php"><span>Kjær Gartneri</span></a></li>
+			</ul>
+		</div>
+
+		<div class="content" id="tools">
+	    	<h3>Tools</h3>
+			<p>This is the list of the tools I use while working on projects and which I have the most experience with</p>
+	    	<ul>
+	    		<li><span>Adobe Photoshop</span></li>
+	    		<li><span>Adobe Illustrator</span></li>
+	    		<li><span>Adobe After Effects</span></li>
+	    		<li><span>Espresso</span></li>
+	    		<li><span>GitHub</span></li>
+	    		<li><span>Wacom Bamboo</span></li>
+	    	</ul>   
+		</div>
+		
+		<div class="content" id="contact">
+			<h3>Contact</h3>
+			<p>You can reach me on one or more of these places</p>
+			<ul>
+				<li><a href="https://no.linkedin.com/pub/anneli-ulsberg/97/287/88"><span>Linkedin</span></a></li>
+				<li><a href="https://www.facebook.com/anneli.ulsberg"><span>Facebook</span></a></li>
+				<li><a href="mailto:anneli@ulsberg.no"><span>mail</span></a></li>
+				<li><a href="https://github.com/anneliulsberg"><span>Github</span></a></li>
+				<li><a href="https://twitter.com/litntweet"><span>Twitter</span></a></li>
+			</ul>	     
+		</div>
+	</div>  	
 		
 	</body>
-	</html>
+</html>
