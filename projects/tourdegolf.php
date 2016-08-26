ace2816462cc0fe543b3db798f16f2d8c1e47e46
anneliulsberg <anneli@ulsberg.no>
2016-08-26 12:39:00 +0200

    Added the portfolio project Tour de Golf

---
+++ projects/tourdegolf.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+
+	<head>
+		<meta charset="utf-8">
+		<title>Anneli - Web and interaction designer</title>
+		<link href='https://fonts.googleapis.com/css?family=Roboto:300,400,500,900' rel='stylesheet' type='text/css'>
+		<link rel="stylesheet" href="../css/tourdegolf.css" type="text/css" />
+		<link rel="icon" type="image/x-icon" href="favicon.ico" />	
+	</head>
+	
+	<body>
+		<?php include '../menu.php'; ?>
+
+		<div id="intro-img">
+			<img src="../images/intro-img-tour-de-golf.jpg" alt="Tour de Golf logo" />
+		</div>
+		
+		<div id="intro">
+			<div id="intro-container">
+				<h1>Tour de Golf</h1>
+			
+				<p>Brochure for Tour de Golf finale in Portugal.</p>
+			</div>
+		</div>
+		
+		<div id="info-container">
+			<article>
+				<h2>About the project</h2>
+				<p>The client needed to get a small A6 brochure designed for the upcoming Tour de Golf finale in Portugal.</p>
+			</article>
+			
+			<article>
+				<h2>Challenges</h2>
+				<p>This small A6 borchure needed to contain a lot of information and the challenge was to get it all in without making it messy.</p>
+			</article>
+			
+			<article>
+				<h2>Solution</h2>
+				<p>To maintain order in this brochure I used colors and pictures to add some air in the content. The text is neatly organised in colums and sections and the logos are sorted after size to balance out all the visual noise.</p>
+			</article>
+			
+		</div>
+		
+		<div id="showcase-img">
+			<img src="../images/showcase-tourdegolf.jpg" alt="Showcase for Tour de Golf" />
+		</div>
+		
+		<div id="project-profile">
+				<div id="project-font">
+					<img src="../images/gotham-font.png" alt="Gotham font" />
+				</div>
+			
+			<div id="project-logo">
+				<img src="../images/tourdegolf-logo.png" alt="Tour de Golf logo" />
+			</div>
+			
+			<div id="color-profile">
+				<ul>
+					<li id="color-1"></li>
+					<li id="color-2"></li>
+					<li id="color-3"></li>
+					<li id="color-4"></li>
+					<li id="color-5"></li>
+				</ul>
+			</div>
+		</div>
+			
+		<div id="process">
+			<img id="process-img-1" src="../images/prosess-projects-tourdegolf-brochure-1.jpg" alt="Front of the brochure" />
+			<img id="process-img-2" src="../images/prosess-projects-tourdegolf-brochure-2.jpg" alt="The back of the brochure" />
+			<img id="process-img-3" src="../images/prosess-projects-tourdegolf-brochure-3.jpg" alt="First page of the brochure" />
+			<img id="process-img-4" src="../images/prosess-projects-tourdegolf-brochure-4.jpg" alt="Second page of the brochure" />
+		</div>
+	</body>
+
+</html>
